662d6fc44bec64b9cf16d772a2235409ccd9a2f1
Mallikarjun Patnam <mallikarjun.patnam@atmecs.com>
2019-06-26 17:43:26 +0530

    Exception handling for Extent reporter log

--- src/main/java/com/cs/automation/util/reporter/ExtentReporter.java
+++ src/main/java/com/cs/automation/util/reporter/ExtentReporter.java
@@ -8,71 +8,91 @@ import org.testng.Reporter;
 
 import com.cs.automation.util.logging.LogManager;
 import com.relevantcodes.extentreports.LogStatus;
+
 /**
- * ReportLogServiceImpl Class contains all type of log services. 
- * And all the methods provide information for log level and reporter level as well
- * which helps user to debug the tests easily even log level will off
+ * ReportLogServiceImpl Class contains all type of log services. And all the
+ * methods provide information for log level and reporter level as well which
+ * helps user to debug the tests easily even log level will off
+ * 
  * @author kaushik.vijay
  *
  */
 public class ExtentReporter implements ReportLogService {
 
-	 Logger logger = null;
-	
-	 /**
-	 * Added a constructor with custom class-name as parameter
-	 * In this method, creating a logManager instance with logger
+	Logger logger = null;
+
+	/**
+	 * Added a constructor with custom class-name as parameter In this method,
+	 * creating a logManager instance with logger
+	 * 
 	 * @param classname
 	 */
-	public ExtentReporter(Class<?> className){
-		logger =  LogManager.getInstance().getLogger(className);
-	}
-	
-	/**
-	 * Debug level helps developer/script developers to debug application.
-	 * This method accepts one argument as string for custom message
-	 * It provides log information of debug level along with reporter log information
-	 * @param message
-	 */
-	public void debug(String message){
-		logger.debug(message);
-		Reporter.log(message);
-		ExtentTestManager.getTest().log(LogStatus.INFO, message);
-	}
-	
-	/**
-	 * Info level gives the progress and chosen state information. 
-	 * This method accepts one argument as string for custom message
-	 * It provides log information of info level along with reporter log information
-	 * @param message
-	 */
-	public void info(String message){
-		logger.info(message);
-		Reporter.log(message);
-		ExtentTestManager.getTest().log(LogStatus.INFO, message);
+	public ExtentReporter(Class<?> className) {
+		logger = LogManager.getInstance().getLogger(className);
 	}
 
 	/**
-	 * Warning level gives a warning about an unexpected event to the user.
-	 * This method accepts one argument as string for custom message
-	 * It provides reporter log information of warn level along with log information
+	 * Debug level helps developer/script developers to debug application. This
+	 * method accepts one argument as string for custom message It provides log
+	 * information of debug level along with reporter log information
+	 * 
 	 * @param message
 	 */
-	public void warning(String message){
+	public void debug(String message) {
+		logger.debug(message);
+		Reporter.log(message);
+		try {
+			ExtentTestManager.getTest().log(LogStatus.INFO, message);
+		} catch (Exception e) {
+		}
+	}
+
+	/**
+	 * Info level gives the progress and chosen state information. This method
+	 * accepts one argument as string for custom message It provides log information
+	 * of info level along with reporter log information
+	 * 
+	 * @param message
+	 */
+	public void info(String message) {
+		logger.info(message);
+		Reporter.log(message);
+		try {
+			ExtentTestManager.getTest().log(LogStatus.INFO, message);
+		} catch (Exception e) {
+		}
+	}
+
+	/**
+	 * Warning level gives a warning about an unexpected event to the user. This
+	 * method accepts one argument as string for custom message It provides reporter
+	 * log information of warn level along with log information
+	 * 
+	 * @param message
+	 */
+	public void warning(String message) {
 		logger.warn(message);
 		Reporter.log(message);
-		ExtentTestManager.getTest().log(LogStatus.WARNING, message);
+		try {
+			ExtentTestManager.getTest().log(LogStatus.WARNING, message);
+		} catch (Exception e) {
+		}
 	}
-	
+
 	/**
-	 * Error level gives information about a serious error which needs to be addressed and may result in unstable state.
-	 * This method accepts one argument as string for custom message
-	 * It provides reporter log information of error level along with log information
-	 * @param message 
+	 * Error level gives information about a serious error which needs to be
+	 * addressed and may result in unstable state. This method accepts one argument
+	 * as string for custom message It provides reporter log information of error
+	 * level along with log information
+	 * 
+	 * @param message
 	 */
-	public void error(String message){
+	public void error(String message) {
 		logger.error(message);
 		Reporter.log(message);
-		ExtentTestManager.getTest().log(LogStatus.ERROR, message);
+		try {
+			ExtentTestManager.getTest().log(LogStatus.ERROR, message);
+		} catch (Exception e) {
+		}
 	}
 }
